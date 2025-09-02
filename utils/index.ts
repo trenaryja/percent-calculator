439f13d51ce7d41e37321311efed3ed7afccc781
Justin Trenary <trenaryja@gmail.com>
2025-09-01 20:38:11 -0400

    add equation

--- utils/index.ts
+++ utils/index.ts
@@ -10,9 +10,9 @@ export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
 
 export const equations: Omit<EquationProps, 'numberOfDecimals'>[] = [
 	{
-		labels: ['x', 'is what % of', 'y'],
-		formula: (x, y) => (x / y) * 100,
-		resultIsPercentage: true,
+		labels: ["What's", 'x', 'plus', 'y', '%'],
+		formula: (x, y) => x + (x * y) / 100,
+		resultIsPercentage: false,
 	},
 	{
 		labels: ["What's the % change from", 'x', 'to', 'y'],
@@ -20,8 +20,13 @@ export const equations: Omit<EquationProps, 'numberOfDecimals'>[] = [
 		resultIsPercentage: true,
 	},
 	{
+		labels: ['x', 'is what % of', 'y'],
+		formula: (x, y) => (x / y) * 100,
+		resultIsPercentage: true,
+	},
+	{
 		labels: ['What is', 'x', '% of', 'y'],
 		formula: (x, y) => (y * x) / 100,
-		resultIsPercentage: true,
+		resultIsPercentage: false,
 	},
 ]
